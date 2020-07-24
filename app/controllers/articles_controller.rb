73789c42fbaba89d40f67aabb1c893585204e78a
Ignatius22 <igee54077@gmail.com>
2020-07-24 21:33:43 +0100

    add all CRUD method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,16 +9,34 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
-		
+	 @article = Article.new
 	end
 
+	def edit
+	  @article = Article.find(params[:id])
+	end
 	
 	def create
 	  @article = Article.new(article_params)
 	  @article.save
 	  redirect_to @article
 	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+   end
  
+   def destroy
+	   @article = Article.find(params[:id])
+	   @article.destroy
+	   redirect_to articles_path
+   end
+
 
 	private
 	def article_params
